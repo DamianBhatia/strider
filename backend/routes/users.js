c3f56cc95810ea12e9540e96a31b818a07fc4e55
aco12365 <aca12365@outlook.com>
2021-09-07 14:22:41 -0400

    blah

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { create } = require('domain');
 const fs = require('fs');
 const {db} =require('../services/dbConnection')
+const {encrypt,dcrypt,test} = require('../services/crypt')
 /**
  * add user to db end point 
  */
@@ -12,11 +13,33 @@ router.route('/add').post((req,res)=>{
         email,
         password,
     }= req.body
+    const hash = encrypt(password);
     var createUser = fs.readFileSync('sql/create_new_user.sql').toString();
-    db.query(createUser,[firstName,lastName,email,password],(err, results) => {
+    db.query(createUser,[firstName,lastName,email,hash],(err, results) => {
         if (err) throw err
         res.status(200).json(results)
     })
+})
+
+router.route('/login/:email/:password').get((req,res)=>{
+    const {
+        email,
+        password,
+    } = req.params
+
+    console.log(req.params)
+    const getUser = fs.readFileSync('sql/get_user.sql').toString()
+    let isGoodCredentials;
+    db.query(getUser, [email],(err,results) => {
+        if (err) throw err
+        const {rows: [{password: hash}]} = results
+        isGoodCredentials = dcrypt(password, hash)
+        res.status(200).json(isGoodCredentials)
+        
+    })
+
+    return isGoodCredentials
+    
 })
 
 module.exports = router;
